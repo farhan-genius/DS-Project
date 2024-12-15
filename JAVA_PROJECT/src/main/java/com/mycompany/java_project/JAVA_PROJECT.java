b47f1a802c7b7bb7634f190d026ab196fd6134ba
farhan-genius <sfarhan316@gmail.com>
2024-12-15 17:21:24 +0500

    Add files via upload

---
+++ JAVA_PROJECT/src/main/java/com/mycompany/java_project/JAVA_PROJECT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_project;
+
+/**
+ *
+ * @author LASHARI
+ */
+public class JAVA_PROJECT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
